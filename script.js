6390ce527d2ca05a508fedcc77e500ee56c2019d
Osei Rick <rickoseiampah@gmail.com>
2022-06-16 18:31:54 +0300

    functions created

--- script.js
+++ script.js
@@ -1,86 +1,85 @@
-let body = document.querySelector('body')
-let frame = document.getElementById('gameFrame')
-let title = document.getElementById('title')
-let Congrats = document.getElementById('Congrats')
-let rock_btn = document.querySelector('#rock');
-let paper_btn = document.getElementById('paper');
-let scissors_btn = document.getElementById('scissors');
-let results = document.querySelector('#results')
-let p1 = document.querySelector('#playerPoints')
-let c1 = document.querySelector('#comPoints')
-
-
 let playerPoints = 0;
 let comPoints = 0;
 
-let items = ['rock', 'paper', 'scissors'];
 
-let srcList = ['com_rock.png', 'com_paper.png', 'com_scissors.png']
 let playerHand = document.getElementById('playerHand')
 let comHand = document.getElementById('comHand')
 
+function choices() {
+    let rock_btn = document.querySelector('#rock');
+    let paper_btn = document.getElementById('paper');
+    let scissors_btn = document.getElementById('scissors');
+    let p1 = document.querySelector('#playerPoints')
+    let c1 = document.querySelector('#comPoints')
 
-rock_btn.addEventListener('click', (e) => {
-    playerChoice = 'rock'
-    randomNum = Math.floor(Math.random() * 3);
-    computerChoice = items[randomNum];
+    let items = ['rock', 'paper', 'scissors'];
 
-    handMovement();
+    let srcList = ['com_rock.png', 'com_paper.png', 'com_scissors.png']
 
-    setTimeout(() => {
-        playerHand.src = '/images/player_rock.png'
-        comHand.src = '/images/' + srcList[randomNum]
-        determineWinner(playerChoice, computerChoice)
-        console.log(computerChoice)
-        p1.innerHTML = `Player Points : ${playerPoints} pts`
-        c1.innerHTML = `Computer Points : ${comPoints} pts`
-    }, 2000)
-});
+    rock_btn.addEventListener('click', (e) => {
+        playerChoice = 'rock'
+        randomNum = Math.floor(Math.random() * 3);
+        computerChoice = items[randomNum];
 
+        handMovement();
 
-
-paper_btn.addEventListener('click', (e) => {
-    playerChoice = 'paper'
-    randomNum = Math.floor(Math.random() * 3);
-    computerChoice = items[randomNum];
-
-    handMovement();
-
-    setTimeout(() => {
-        playerHand.src = '/images/player_paper.png'
-        comHand.src = '/images/' + srcList[randomNum]
-        determineWinner(playerChoice, computerChoice)
-        console.log(computerChoice)
-        p1.innerHTML = `Player Points : ${playerPoints} pts`
-        c1.innerHTML = `Computer Points : ${comPoints} pts`
-    }, 2000)
-
-});
+        setTimeout(() => {
+            playerHand.src = '/images/player_rock.png'
+            comHand.src = '/images/' + srcList[randomNum]
+            determineWinner(playerChoice, computerChoice)
+            console.log(computerChoice)
+            p1.innerHTML = `Player Points : ${playerPoints} pts`
+            c1.innerHTML = `Computer Points : ${comPoints} pts`
+        }, 2000)
+    });
 
 
 
-scissors_btn.addEventListener('click', (e) => {
-    playerChoice = 'scissors'
-    randomNum = Math.floor(Math.random() * 3);
-    computerChoice = items[randomNum];
+    paper_btn.addEventListener('click', (e) => {
+        playerChoice = 'paper'
+        randomNum = Math.floor(Math.random() * 3);
+        computerChoice = items[randomNum];
 
-    handMovement();
+        handMovement();
 
-    setTimeout(() => {
-        playerHand.src = '/images/player_scissors.png'
-        comHand.src = '/images/' + srcList[randomNum]
-        determineWinner(playerChoice, computerChoice)
-        console.log(computerChoice)
-        p1.innerHTML = `Player Points : ${playerPoints} pts`
-        c1.innerHTML = `Computer Points : ${comPoints} pts`
-    }, 2000)
-});
+        setTimeout(() => {
+            playerHand.src = '/images/player_paper.png'
+            comHand.src = '/images/' + srcList[randomNum]
+            determineWinner(playerChoice, computerChoice)
+            console.log(computerChoice)
+            p1.innerHTML = `Player Points : ${playerPoints} pts`
+            c1.innerHTML = `Computer Points : ${comPoints} pts`
+        }, 2000)
+
+    });
 
 
 
+    scissors_btn.addEventListener('click', (e) => {
+        playerChoice = 'scissors'
+        randomNum = Math.floor(Math.random() * 3);
+        computerChoice = items[randomNum];
 
+        handMovement();
+
+        setTimeout(() => {
+            playerHand.src = '/images/player_scissors.png'
+            comHand.src = '/images/' + srcList[randomNum]
+            determineWinner(playerChoice, computerChoice)
+            console.log(computerChoice)
+            p1.innerHTML = `Player Points : ${playerPoints} pts`
+            c1.innerHTML = `Computer Points : ${comPoints} pts`
+        }, 2000)
+    });
+
+};
 
 function determineWinner(playerChoice, computerChoice) {
+    let frame = document.getElementById('gameFrame');
+    let title = document.getElementById('title');
+    let Congrats = document.getElementById('Congrats');
+    let results = document.getElementById('results');
+
     if (playerChoice === computerChoice) {
         results.innerHTML = 'It is a tie!'
     }
@@ -119,6 +118,7 @@ function determineWinner(playerChoice, computerChoice) {
         Congrats.style.display = 'block'
         Congrats.textContent = 'Player Wins Overall !'
         title.style.color = 'rgb(209, 170, 96)'
+        results.style.color = 'rgb(225, 191, 129)'
         setTimeout(() => {
             window.location.reload();
         }, 7000)
@@ -129,6 +129,8 @@ function determineWinner(playerChoice, computerChoice) {
         Congrats.style.display = 'block'
         Congrats.textContent = 'Com Wins Overall !'
         title.style.color = 'rgb(225, 191, 129)'
+        results.style.color = 'rgb(225, 191, 129)'
+
         setTimeout(() => {
             window.location.reload();
         }, 7000)
@@ -137,17 +139,19 @@ function determineWinner(playerChoice, computerChoice) {
 
 
 function handMovement() {
+    playerHand.addEventListener('animationend', function () {
+        this.classList.remove('move')
+    });
     playerHand.classList.add('move')
+
     comHand.classList.add('move')
+    comHand.addEventListener('animationend', function () {
+        this.classList.remove('move')
+    });
 }
+
+choices()
 
 function playAgain() {
     window.location.reload();
-}
-
-
-
-
-
-
-
+};
